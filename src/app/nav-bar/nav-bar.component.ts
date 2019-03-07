1113c2cfd9fadb11c399af1185b91e6cd65f6684
michele byman <michelebyman@gmail.com>
2019-03-07 16:17:45 +0100

    Logout link added

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 // showes the navbar
 @Component({
   selector: 'app-nav-bar',
@@ -7,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private authservice: AuthService) {
+
+  }
 
   ngOnInit() {
   }
+
 }
